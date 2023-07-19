c26ba35b600318532aa56b0822aab35fe3ccfb0a
Gabriel Hurtado <102824698+Ghurt007@users.noreply.github.com>
2023-07-19 12:29:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import string
 from flask import Flask
 from book_browsing_sorting_api.browse import browse
 from flask_sqlalchemy import SQLAlchemy
-from database import Wishlist, db,migrate, BooksTesting, BookDetails
+from database import Wishlist, db,migrate, BooksTesting, BookDetails, AuthorDetails
 # Import your route file here. Remember to rename the folder to remove the number and '_' in front 
 from book_details_api.book_details import book_details
 from book_rating_commenting_api.book_rating import book_rating
@@ -34,21 +34,27 @@ with app.app_context():
         # Populate Fields
         # populate_books = BooksTesting(name="Star Wars", book_detail="Hello detail")
 
+
+        author_dummy = AuthorDetails(author_fn = "Ted", author_ln = "Gioia", biography="Ted Gioia is an American jazz critic and music historian. He is author of eleven books",
+                                     publisher = "Oxford University Press")
+
+        db.session.add(author_dummy)
+
         for i in range(6):
             wishlist_db = Wishlist(userId=i, name=''.join(random.choices(string.ascii_letters, k=5)))
             db.session.add(wishlist_db)
         # TODO: Name should not be unique neither primary key. Cause issues when running the app.     
         # Create your model instance here and populate fields
-        # populate_books = BookDetails(book_name="The History of Jazz", ISBN="9780190087210", book_description="An updated new edition of Ted Gioia's universally acclaimed history of jazz, with a wealth of new insight on this music's past, present, and future.",
-        #                              book_price=24.95, author="Ted Gioia", genre="Music", year_published=2021, copies_sold=58042)
-        
+
+
         # Add your values to the database here
-        # db.session.add(populate_books)
+
         # It commits or saves all fileds to the database
         db.session.commit()
     
     except Exception as e:
         print( f'There was an error: {str(e)}')
+
 
 
 
